1aba4b63e7f6164a7bd38fc74234942108f81c92
shanfaradbautista <99014666+shanfaradbautista@users.noreply.github.com>
2022-03-01 19:09:57 +0800

    Update TabOneScreen.tsx

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,17 +1,55 @@
-import { StyleSheet } from 'react-native';
+import React, { Component } from 'react';
+import { Alert, Button, Text, TouchableOpacity, TextInput, View, StyleSheet, ImageBackground } from 'react-native';
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
+export default class App extends Component {
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
-  );
+    state = {
+      email: '',
+      password: '',
+    };
+  
+  
+  onLogin() {
+    const { email, password } = this.state;
+
+    Alert.alert('Credentials', `email: ${email} + password: ${password}`);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.titleText}>Hello!</Text>
+        <Text style={styles.titleText}>Welcome back you've been missed!</Text>
+        <TextInput
+          value={this.state.email}
+          keyboardType = 'email-address'
+          onChangeText={(email) => this.setState({ email })}
+          placeholder='Enter Username'
+          placeholderTextColor = 'white'
+          style={styles.input}
+        />
+        <TextInput
+          value={this.state.password}
+          onChangeText={(password) => this.setState({ password })}
+          placeholder={'Enter Password'}
+          secureTextEntry={true}
+          placeholderTextColor = 'white'
+          style={styles.input}
+        />
+        
+     
+        <TouchableOpacity
+          style={styles.button}
+          onPress={this.onLogin.bind(this)}
+       >
+         <Text style={styles.buttonText}> LOG IN </Text>
+       </TouchableOpacity>
+        
+       <Text style={styles.titleText}>Forgot Password?</Text>
+      </View>
+      
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -19,14 +57,42 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'teal',
+    
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  titleText:{
+    fontFamily: 'SpaceMono-Regular.ttf',
+    fontSize: 16,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  button: {
+    alignItems: 'center',
+    backgroundColor: 'teal',
+    width: 120,
+    height: 45,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'white',
+    borderRadius: 15,
+    marginBottom: 10,
+  },
+  buttonText:{
+    fontFamily: 'SpaceMono-Regular.ttf',
+    fontSize: 16,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    width: 250,
+    fontFamily: 'SpaceMono-Regular.ttf',
+    fontSize: 15,
+    height: 50,
+    padding: 10,
+    borderWidth: 3,
+    borderColor: 'black',
+    marginVertical: 10,
+    alignItems: 'center',
+    justifyContent: 'center'
   },
 });
